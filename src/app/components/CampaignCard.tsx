8831279f18e1fa106ce5353619c61d5d4e1966ca
nurrrrrrba <nurik23022001@gmail.com>
2025-04-25 11:10:26 +0500

    fixed button

--- src/app/components/CampaignCard.tsx
+++ src/app/components/CampaignCard.tsx
@@ -68,7 +68,7 @@ export default function CampaignCard({campaignAddress}:CampaignCardProps) {
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{campaignDescription}</p>
             </div>
             <Link href={`/campaign/${campaignAddress}`} passHref={true}>
-                <p className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg:blue-800">
+                <p className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
                     View Campaign
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
